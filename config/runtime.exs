7ebb9ab66d049b16f3d64df3016aee69c7c5886d
Matheus <cardosaum@pm.me>
2024-01-26 17:20:38 -0300

    Disable origin check and update URL configuration in runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -54,6 +54,7 @@ if config_env() == :prod do
   config :tarragon, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :tarragon, TarragonWeb.Endpoint,
+    check_origin: false,
     url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
