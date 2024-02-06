46910ac8e1c80c1db999261ba33b9f7ecf97d14b
Roman Berdichevskii <r378ut@gmail.com>
2024-02-06 17:07:07 +0100

    Update lib/tarragon/message/chat_room_member.ex

--- lib/tarragon/message/chat_room_member.ex
+++ lib/tarragon/message/chat_room_member.ex
@@ -7,7 +7,7 @@ defmodule Tarragon.Message.ChatRoomMember do
   schema "chat_room_member" do
 
     belongs_to :chat_room, ChatRoom
-    belongs_to :user_characters, UserCharacter
+    belongs_to :user_character, UserCharacter
     has_many :user_character_messages, UserCharacterMessage
 
   timestamps()
