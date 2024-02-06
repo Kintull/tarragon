a2c19db8ea695cc5fdb1cedb08f2f4c710e9358d
MichalJankowsk <michal.jankowski003@gmail.com>
2024-02-06 03:11:31 -0500

    add world chat message model and migration

---
+++ lib/tarragon/message/user_character_messages.ex
@@ -0,0 +1,16 @@
+defmodule Tarragon.Message.UserCharacterMessage do
+  use Ecto.Schema
+  alias Tarragon.Accounts.UserCharacter
+  alias Tarragon.Message.ChatRoomMember
+  alias Tarragon.Message.ChatRoom
+
+  schema "user_character_message" do
+
+  field :message, :string
+  belongs_to :user_character, UserCharacter
+  belongs_to :chat_room, ChatRoom
+  belongs_to :chat_member, ChatRoomMember
+  timestamps()
+
+  end
+end
