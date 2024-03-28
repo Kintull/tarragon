fed9e77f6012c79ac5e9ff9c84ff0e79f52237ef
Roman Berdichevskii <r378ut@gmail.com>
2024-03-28 23:14:32 +0100

    wip

--- lib/tarragon_web/live/page_live/battle_screen_v3.ex
+++ lib/tarragon_web/live/page_live/battle_screen_v3.ex
@@ -11,8 +11,11 @@ defmodule TarragonWeb.PageLive.BattleScreenV3 do
     outer_r = 20
     inner_r = round(outer_r * 0.86602540378)
 
-    grid = generate_grid_circle(3, 20)
-    grid = generate_grid_rectangle(5, 3, 20)
+    cell_width = 63
+#    grid = generate_grid_rectangle_flat(9, 5, cell_width)
+#    grid = generate_grid_rectangle_pointy(9, 5, cell_width)
+#    grid = generate_grid_circle_pointy(3, cell_width)
+    grid = generate_grid_circle_flat(5, cell_width)
 
     IO.inspect(grid)
     socket = assign(socket, grid: grid)
@@ -24,29 +27,42 @@ defmodule TarragonWeb.PageLive.BattleScreenV3 do
     Enum.max([abs(hex_a.x - hex_b.x), abs(hex_a.y - hex_b.y), abs(hex_a.z - hex_b.z)])
   end
 
-  def hex_to_offset(hex) do
+  def hex_to_offset_pointy(hex) do
     x = hex.x + div(hex.y, 2)
     y = hex.y
     %{x: x, y: y}
   end
 
-  def offset_to_hex(offset) do
+  def offset_to_hex_pointy(offset) do
     x = offset.x - div(offset.y, 2)
     y = offset.y
     z = -x - y
     %{x: x, y: y, z: z}
   end
 
-  def generate_grid_circle(map_radius, outer_hex_radius) do
-    inner_r = round(outer_hex_radius * 0.86602540378)
-    for x <- -map_radius..map_radius, y <- -map_radius..map_radius do
+  def offset_to_hex_flat(offset) do
+    x = offset.x
+    y = offset.y - div(offset.x, 2)
+    z = -x - y
+    %{x: x, y: y, z: z}
+  end
+
+  def generate_grid_circle_pointy(map_radius, cell_width) do
+    ratio = 1.278688524590164
+
+    cell_height = (cell_width / ratio) |> round
+
+    inner_r = (cell_width / 2) |> round
+    outer_r = (cell_height / 2) |> round
+
+    cells = for x <- -map_radius..map_radius, y <- -map_radius..map_radius do
       offset_coords = %{x: x, y: y}
-      hex_coordinates = offset_to_hex(offset_coords)
+      hex_coordinates = offset_to_hex_pointy(offset_coords)
 
       if distance(%{x: 0, y: 0, z: 0}, hex_coordinates) <= map_radius do
         left = ((x + y * 0.5 - div(y,2)) * (inner_r * 2)) |> round
-        top = (y * outer_hex_radius * 1.5) |> round
-        height = (outer_hex_radius * 2) |> round
+        top = (y * outer_r * 1.5) |> round
+        height = (outer_r * 2) |> round
         width = (inner_r * 2) |> round
         %{coord: offset_coords, hex_coords: hex_coordinates, left: left, top: top, width: width, height: height}
       else
@@ -54,26 +70,99 @@ defmodule TarragonWeb.PageLive.BattleScreenV3 do
       end
     end
     |> Enum.reject(&is_nil/1)
+
+    %{name: "circular_grid_pointy", cells: cells, width: map_radius * cell_width, height: map_radius * cell_height}
   end
 
-  def generate_grid_rectangle(height_cells, width_cells, outer_hex_radius) do
-    inner_r = round(outer_hex_radius * 0.86602540378)
-    for x <- 0..width_cells, y <- 0..height_cells do
-#      if div(y, 2) == 0 and x == width_cells do
-#        nil
-#      else
-#      end
-        offset_coords = %{x: x, y: y}
-        hex_coordinates = offset_to_hex(offset_coords)
+  def generate_grid_rectangle_pointy(height_cells, width_cells, cell_width) do
+    ratio = 1.278688524590164
 
-        left = ((x + y * 0.5 - div(y,2)) * (inner_r * 2)) |> round
-        top = (y * outer_hex_radius * 1.5) |> round
-        height = (outer_hex_radius * 2) |> round
-        width = (inner_r * 2) |> round
-        %{coord: offset_coords, hex_coords: hex_coordinates, left: left, top: top, width: width, height: height}
+    cell_height = (cell_width / ratio) |> round
+
+    inner_r = (cell_width / 2) |> round
+    outer_r = (cell_height / 2) |> round
+
+    cells = for x <- 0..width_cells-1, y <- 0..height_cells-1 do
+
+        if rem(y,2) != 0 and x == width_cells-1 do
+           # make it symmetrical by removing extra cells on the right on every odd row
+          nil
+        else
+          offset_coords = %{x: x, y: y}
+          hex_coordinates = offset_to_hex_pointy(offset_coords)
+
+          left = ((x + y * 0.5 - div(y,2)) * (inner_r * 2)) |> round
+          top = (y * outer_r * 1.5) |> round
+          height = cell_height
+          width = cell_width
+          %{coord: offset_coords, hex_coords: hex_coordinates, left: left, top: top, width: width, height: height}
+        end
     end
     |> Enum.reject(&is_nil/1)
+
+    %{name: "rectangular_grid_pointy", cells: cells, width: width_cells * cell_width, height: height_cells * cell_height}
   end
+
+  def generate_grid_circle_flat(map_radius, cell_width) do
+    ratio = 1.6
+
+    cell_height = (cell_width / ratio) |> round
+
+    inner_r = (cell_height / 2) |> round
+    outer_r = (cell_width / 2) |> round
+
+
+    cells = for x <- -map_radius..map_radius, y <- -map_radius..map_radius do
+              offset_coords = %{x: x, y: y}
+              hex_coordinates = offset_to_hex_flat(offset_coords)
+
+              if (distance(%{x: 0, y: 0, z: 0}, hex_coordinates) <= map_radius) and (x <= 3 and x >= -3) do
+                left = (x * outer_r * 1.5) |> round
+                top = ((y + x * 0.5 - div(x,2)) * inner_r * 2) |> round
+                height = cell_height
+                width = cell_width
+                %{coord: offset_coords, hex_coords: hex_coordinates, left: left, top: top, width: width, height: height}
+              else
+                nil
+              end
+            end
+            |> Enum.reject(&is_nil/1)
+
+    map_diameter = Enum.map(-map_radius..map_radius, & &1) |> length
+    width = map_diameter * 1.5 * cell_width
+    height = map_diameter * cell_height
+    %{name: "circular_grid_flat", cells: cells, width: width, height: height}
+  end
+
+
+  def generate_grid_rectangle_flat(height_cells, width_cells, cell_width) do
+    ratio = 1.278688524590164
+
+    cell_height = (cell_width * ratio) |> round
+
+    inner_r = (cell_height / 2) |> round
+    outer_r = (cell_width / 2) |> round
+
+    cells = for x <- 0..width_cells-1, y <- 0..height_cells-1 do
+              if rem(x,2) != 0 and y == height_cells-1 do
+                # make it symmetrical by removing extra cells on the right on every odd row
+                nil
+              else
+                offset_coords = %{x: x, y: y}
+                hex_coordinates = offset_to_hex_flat(offset_coords)
+
+                left = (x * outer_r * 1.5) |> round
+                top = ((y + x * 0.5 - div(x,2)) * inner_r * 2) |> round
+                height = cell_height
+                width = cell_width
+                %{coord: offset_coords, hex_coords: hex_coordinates, left: left, top: top, width: width, height: height}
+              end
+            end
+            |> Enum.reject(&is_nil/1)
+
+    %{name: "rectangular_grid_flat", cells: cells, width: width_cells * cell_width, height: height_cells * cell_height}
+  end
+
 
 
 end
